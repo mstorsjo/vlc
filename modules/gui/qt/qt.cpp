b81a3a60af5c3f312812e6aa20ed2463eb8513d9
Pierre Lamot <pierre@videolabs.io>
2019-01-16 14:48:12 +0100

    qt: load static qml plugins

--- modules/gui/qt/qt.cpp
+++ modules/gui/qt/qt.cpp
@@ -67,10 +67,21 @@ extern "C" char **environ;
 
 #ifdef QT_STATIC /* For static builds */
  #include <QtPlugin>
+ #include <QQuickWindow>
 
  #ifdef QT_STATICPLUGIN
   Q_IMPORT_PLUGIN(QSvgIconPlugin)
   Q_IMPORT_PLUGIN(QSvgPlugin)
+  Q_IMPORT_PLUGIN(QJpegPlugin)
+  Q_IMPORT_PLUGIN(QtQuick2Plugin)
+  Q_IMPORT_PLUGIN(QtQuickControls2Plugin)
+  Q_IMPORT_PLUGIN(QtQuickLayoutsPlugin)
+  Q_IMPORT_PLUGIN(QtQuick2WindowPlugin)
+  Q_IMPORT_PLUGIN(QtQuickTemplates2Plugin)
+  Q_IMPORT_PLUGIN(QtQmlModelsPlugin)
+  Q_IMPORT_PLUGIN(QtGraphicalEffectsPlugin)
+  Q_IMPORT_PLUGIN(QtGraphicalEffectsPrivatePlugin)
+
   #if !HAS_QT56
    Q_IMPORT_PLUGIN(AccessibleFactory)
   #endif
